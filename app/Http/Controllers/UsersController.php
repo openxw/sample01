2494cbf0faf6680d84ef77fdc6ad9dba649db103
openxw <openxw@qq.com>
2018-10-29 22:34:45 +0800

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,12 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+//use App\Http\Requests;
+use App\Models\User;
 
 class UsersController extends Controller
 {
-    //
+    //创建新用户
     public function create()
     {
         return view('users.create');
     }
+
+    //显示用户
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        # code...
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'email' => 'required|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+         $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
+    }
+
 }
