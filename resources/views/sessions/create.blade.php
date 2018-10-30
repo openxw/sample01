dcdb1b2edb3444d30b924e2259e637e0b9a65a09
openxw <openxw@qq.com>
2018-10-30 20:43:35 +0800

    Finish login and logout

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -23,10 +23,9 @@
             <input type="password" name="password" class="form-control" value="{{ old('password') }}">
           </div>
 
-           <div class="checkbox">
+          <div class="checkbox">
             <label><input type="checkbox" name="remember"> 记住我</label>
           </div>
-
 
           <button type="submit" class="btn btn-primary">登录</button>
       </form>
