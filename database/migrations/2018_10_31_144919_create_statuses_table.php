3cfc838e6def4c71aa591c4b5055552285bfbebb
openxw <openxw@qq.com>
2018-11-06 17:29:28 +0800

    update

--- database/migrations/2018_10_31_144919_create_statuses_table.php
+++ database/migrations/2018_10_31_144919_create_statuses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateStatusersTable extends Migration
+class CreateStatusesTable extends Migration
 {
     /**
      * Run the migrations.
